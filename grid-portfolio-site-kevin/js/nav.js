34af21968c1512bc066f8ac92e0861f2ff4191ad
Amanpreet kaur <kauramanpreet1199@gmail.com>
2021-07-06 23:56:54 +0530

    completed the navigation bar

--- grid-portfolio-site-kevin/js/nav.js
+++ grid-portfolio-site-kevin/js/nav.js
@@ -1,14 +1,17 @@
-const navToggle = document.querySelector('.menu-button');
-const nav = document.querySelector('nav');
+const navToggle = document.querySelector('.menu-button')
+const nav = document.querySelector('nav')
+const containerAll = document.querySelector('.container-all')
 
-navToggle.addEventListener('click', function (){
-    document.body.classList.toggle('nav-is-open')
-    // arrow function example: https://www.w3schools.com/js/js_arrow_function.asp
+const containerAllstyle = containerAll.style
+const bodyClassList = document.body.classList
+
+navToggle.addEventListener('click', function () {
+	//containerAllstyle.transition = 'transform 250ms ease-in-out' in  _nav.scss file we need to remove the code for transition for containerAll class
+	bodyClassList.toggle('nav-is-open')
+	// arrow function example: https://www.w3schools.com/js/js_arrow_function.asp
 })
 
-
-
-nav.addEventListener('click', function()
-{
-    document.body.classList.remove('nav-is-open')
+nav.addEventListener('click', function () {
+	//containerAllstyle.transition = '0ms'
+	bodyClassList.remove('nav-is-open')
 })
